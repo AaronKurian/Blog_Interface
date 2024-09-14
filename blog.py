8e8fad26d2bfed287a4c9012f59165822431da38
AaronKurian <aaronkurianabraham@gmail.com>
2024-09-14 23:27:54 +0530

    created user login

--- blog.py
+++ blog.py
@@ -21,3 +21,18 @@ def register():
     users[username] = encrypted_password
     blogs[username] = []  # Initialize an empty list for the user's blogs
     print(f"User '{username}' registered successfully!")
+
+    #User Login:
+def login():
+    username = input("Enter username: ")
+    if username not in users:
+        print("Username not found!")
+        return None
+
+    password = getpass.getpass("Enter password: ")
+    if bcrypt.checkpw(password.encode(), users[username]):
+        print(f"Welcome, {username}!")
+        return username
+    else:
+        print("Incorrect password!")
+        return None
