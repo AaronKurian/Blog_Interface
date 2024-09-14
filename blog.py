f1d1fcf0b98542f13a55af088e265ca0cbaab86d
AaronKurian <aaronkurianabraham@gmail.com>
2024-09-14 23:27:17 +0530

    created user registration

--- blog.py
+++ blog.py
@@ -6,3 +6,18 @@ users = {}
 
 # Store blog posts for each user
 blogs = {}
+
+#User Registration:
+
+def register():
+    username = input("Enter a new username: ")
+    if username in users:
+        print("User already exists!")
+        return
+
+    password = getpass.getpass("Enter a new password: ")
+    encrypted_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+    users[username] = encrypted_password
+    blogs[username] = []  # Initialize an empty list for the user's blogs
+    print(f"User '{username}' registered successfully!")
